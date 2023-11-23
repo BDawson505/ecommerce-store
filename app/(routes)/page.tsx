055487ea0e554d221e903545e8ef32149cd00c1b
Bradley Dawson Development <BDawson@Bradley.local>
2023-11-23 15:12:26 +0000

    display billboard from billboard id

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,16 @@
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("bb501e93-8424-4d2d-94dd-6eb068a2de15");
+
   return (
     <Container>
       <div className='space-y-10 pb-10'>
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
