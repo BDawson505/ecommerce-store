4fc0d64e54c12f8400b6db20e26755dc67a44f68
Bradley Dawson Development <BDawson@LG226.lowes.local>
2023-11-24 16:54:53 +0000

    added the product information section to the individual product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 
 import Container from "@/components/ui/container";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 
 interface ProductPageProps {
@@ -25,8 +26,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
             <Gallery images={product.images} />
 
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
-              {/* Info */}
-              Info
+              <Info data={product} />
             </div>
           </div>
 
